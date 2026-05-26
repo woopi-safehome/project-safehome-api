728034ef6dee4db0e79a8413cecc8bd629ed2bbb
woopi1030 <lozenkrantz@gmail.com>
2026-05-26 23:59:50 +0900

    [CHG] Fcm토큰 삭제 메서드에 Transactional 추가

--- src/main/kotlin/com/woopi/safehome/domain/deed/adapter/outbound/UserDeviceQueryAdapter.kt
+++ src/main/kotlin/com/woopi/safehome/domain/deed/adapter/outbound/UserDeviceQueryAdapter.kt
@@ -3,6 +3,7 @@ package com.woopi.safehome.domain.deed.adapter.outbound
 import com.woopi.safehome.domain.auth.adapter.outbound.persistence.jpa.UserDeviceRepository
 import com.woopi.safehome.domain.deed.application.port.outbound.UserDeviceQueryPort
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 class UserDeviceQueryAdapter(
@@ -12,6 +13,7 @@ class UserDeviceQueryAdapter(
     override fun findTokensByUserId(userId: Long): List<String> =
         userDeviceRepository.findAllByUserId(userId).map { it.fcmToken }
 
+    @Transactional
     override fun deleteByFcmToken(fcmToken: String) =
         userDeviceRepository.deleteByFcmToken(fcmToken)
 }
